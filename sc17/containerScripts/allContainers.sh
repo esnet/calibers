26f490a5adfb5cba4c6ecc5baed8b16a25250568
Nathan Hanford <nhanford@es.net>
2017-11-11 13:47:18 -0700

    new script for a shaped example

--- sc17/containerScripts/allContainers.sh
+++ sc17/containerScripts/allContainers.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in 111 112 113 114 115 116
+for i in 111 112 113 114 115 116 118
 do
-	ssh rootnh@192.168.120.$i rm /storage/*.img
+	ssh rootnh@192.168.120.$i  << EOF
+pkill gridftp
+globus-gridftp-server -S -p 8$i -data-interface 192.168.200.$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/$i.log -log-level all
+EOF
 done
