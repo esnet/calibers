86eb72b8374627b31df4410909062cb485cf55e1
Nathan Hanford <nhanford@es.net>
2017-11-11 13:50:35 -0700

    new demo file

--- sc17/demoPaced.sh
+++ sc17/demoPaced.sh
@@ -2,13 +2,13 @@
 
 
 # create junk files, start servers
-for i in 111 112 113 114 115 116
+for i in 190 191 192 194 200 201 118
 do
 	echo "*******First contact to $i"
     ssh rootnh@192.168.120.$i << EOF
 pkill gridftp
 ps aux | grep gridftp
-globus-gridftp-server -S -p 8$i -data-interface 192.168.200.$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/$i.log -log-level all
+globus-gridftp-server -S -p 8$i -data-interface 192.168.112.$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/$i.log -log-level all
 ps aux | grep gridftp
 ifconfig eth1 mtu 9000
 tc qdisc del dev eth1 root
@@ -16,79 +16,86 @@ tc qdisc show dev eth1
 EOF
 done
 # AMST
-ssh rootnh@192.168.120.111 << EOF
+ssh rootnh@192.168.120.190 << EOF
 if [ ! -e "/storage/zero.img" ]
 then
 	dd if=/dev/zero of=/storage/zero.img bs=1M count=2145
 	chmod +r /storage/zero.img
 fi
 ls /storage | grep img
+tc qdisc add dev eth1 root fq
+tc qdisc change dev eth1 root fq maxrate 171000002bit
+tc qdisc show
 EOF
 # CERN
-ssh rootnh@192.168.120.112 << EOF
+ssh rootnh@192.168.120.194 << EOF
 if [ ! -e "/storage/zero.img" ]
 then
 	dd if=/dev/zero of=/storage/zero.img bs=1M count=1609
 	chmod +r /storage/zero.img
 fi
 ls /storage | grep img
+tc qdisc add dev eth1 root fq
+tc qdisc change dev eth1 root fq maxrate 149955555bit
+tc qdisc show
 EOF
 # AOFA
-ssh rootnh@192.168.120.114 << EOF
+ssh rootnh@192.168.120.191 << EOF
 if [ ! -e "/storage/zero.img" ]
 then
 	dd if=/dev/zero of=/storage/zero.img bs=1M count=429
 	chmod +r /storage/zero.img
 fi
 ls /storage | grep img
+tc qdisc add dev eth1 root fq
+tc qdisc change dev eth1 root fq maxrate 99777777bit
+tc qdisc show
 EOF
 # ATLA
-ssh rootnh@192.168.120.113 << EOF
+ssh rootnh@192.168.120.200 << EOF
 if [ ! -e "/storage/zero.img" ]
 then
 	dd if=/dev/zero of=/storage/zero.img bs=1M count=178
 	chmod +r /storage/zero.img
 fi
 ls /storage | grep img
+tc qdisc add dev eth1 root fq
+tc qdisc change dev eth1 root fq maxrate 49600000bit
+tc qdisc show
 EOF
 # WASH
-ssh rootnh@192.168.120.115 << EOF
+ssh rootnh@192.168.120.192 << EOF
 if [ ! -e "/storage/zero.img" ]
 then
 	dd if=/dev/zero of=/storage/zero.img bs=1M count=85
 	chmod +r /storage/zero.img
 fi
 ls /storage | grep img
+tc qdisc add dev eth1 root fq
+tc qdisc change dev eth1 root fq maxrate 29666666bit
+tc qdisc show
 EOF
 # DENV
-ssh rootnh@192.168.120.116 << EOF
+ssh rootnh@192.168.120.201 << EOF
 if [ ! -e "/storage/zero.img" ]
 then
 	dd if=/dev/zero of=/storage/zero.img bs=1M count=57
 	chmod +r /storage/zero.img
 fi
 ls /storage | grep img
+tc qdisc add dev eth1 root fq
+tc qdisc change dev eth1 root fq maxrate 19666666bit
+tc qdisc show
 EOF
-
-
-echo "*******Contacting receiving server"
-
-scp alias-file xfer-file rootnh@192.168.120.118:~
-
-ssh rootnh@192.168.120.118 << EOF
-ifconfig eth1 mtu 9000
-ps aux | grep gridftp
-EOF
-#globus-gridftp-server -S -p 8118 -data-interface 192.168.200.118 -aa -anonymous-user 'nhanford' -home-dir / -Z ~/118.log -log-level all
 
 echo "*******Initiatiing Transfer"
-time globus-url-copy -cc 6 -p 1 -af alias-file -f xfer-file
+time globus-url-copy -cc 6 -p 1 -af calibersAliasFile -f xfer-file
 
 d=$(date +%F-%H-%M)
 mkdir ~/$d
 
 # move logs, stop servers
-for i in 111 112 113 114 115 116 118
+for i in 190 191 192 194 200 201 118
 do
 	echo "********Third contact to $i"
 	scp rootnh@192.168.120.$i:~/$i.log ~/$d
