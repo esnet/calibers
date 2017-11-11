fffef5be3b211b8275c80f43e8f2d425d8382201
Nathan Hanford <nhanford@es.net>
2017-11-11 11:18:06 -0700

    running gridftp nonstop now

--- sc17/demoUnpaced.sh
+++ sc17/demoUnpaced.sh
@@ -13,7 +13,7 @@ globus-gridftp-server -S -p 8$i -data-interface 192.168.200.$i -aa -anonymous-us
 ps aux | grep gridftp
 EOF
 done
-
+globus-gridftp-server -S -p 8$i -data-interface 192.168.200.$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/$i.log -log-level all
 # AMST
 ssh rootnh@192.168.120.111 << EOF
 if [ ! -e "/storage/zero.img" ]
@@ -76,9 +76,10 @@ scp alias-file xfer-file rootnh@192.168.120.118:~
 
 ssh rootnh@192.168.120.118 << EOF
 ifconfig eth1 mtu 9000
-globus-gridftp-server -S -p 8118 -data-interface 192.168.200.118 -aa -anonymous-user 'nhanford' -home-dir / -Z ~/118.log -log-level all
 ps aux | grep gridftp
 EOF
+#globus-gridftp-server -S -p 8118 -data-interface 192.168.200.118 -aa -anonymous-user 'nhanford' -home-dir / -Z ~/118.log -log-level all
+
 echo "*******Initiatiing Transfer"
 time globus-url-copy -cc 6 -p 1 -af alias-file -f xfer-file
 
@@ -93,10 +94,10 @@ do
 	ssh rootnh@192.168.120.$i << EOF
 mkdir ~/$d
 mv *.log ~/$d
-pkill gridftp
 ps aux | grep gridftp
 EOF
 done
+#pkill gridftp
 
 cp results.py ~/$d
 cd ~/$d
